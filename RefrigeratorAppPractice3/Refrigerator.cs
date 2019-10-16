dde6dfc063565ddb208196cba8b6ea5513f13568
Md. Habibur Rahman <habibcsepust@gmail.com>
2019-10-16 12:40:10 +0600

    Added

---
+++ RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigerator
+    {
+        public double max;
+        List<double> Items = new List<double>();
+        List<double> Units = new List<double>();
+        List<double> Weights = new List<double>();
+        private double currW = 0;
+        private double item = 0;
+        private double unit = 0;
+        private double weight = 0;
+        private double remainingWeight;
+        private double currentWeight;
+        private double totalWeight = 0;
+
+
+        public Refrigerator(double maxValue)
+        {
+            max = maxValue;
+        }
+
+        public bool Set(double item,double weight)
+        {
+            currentWeight = item * weight;
+            totalWeight = totalWeight + currentWeight;
+            bool isOverflow = false;
+            if (totalWeight < max)
+            {
+                Items.Add(item);
+                Units.Add(weight);
+                Weights.Add(currentWeight);
+            }
+            else
+            {
+                isOverflow = true;
+                totalWeight = totalWeight - currentWeight;
+            }
+
+            return isOverflow;
+        }
+
+
+        public double GetCurrentWeight()
+        {
+            return currentWeight;
+        }
+
+        public double GetWeight()
+        {
+            weight = 0;
+
+            foreach (var CW in Weights)
+            {
+               weight = weight + CW;
+            }
+            return weight;
+        }
+
+        public double GetItems()
+        {
+            item = 0;
+
+            foreach (var itm in Items)
+            {
+                item = item + itm;
+            }
+            return item;
+        }
+
+        public double GetUnit()
+        {
+            unit = 0;
+
+            foreach (var unt in Units)
+            {
+                unit = unit + unt;
+            }
+            return unit;
+        }
+
+        public double RemainingWeight()
+        {
+            remainingWeight = max - GetWeight();
+            return remainingWeight;
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        string showTotal = "";
+        int i = 1;
+
+        Refrigerator refrigerator;
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+
+            if (refrigerator.Set(Convert.ToDouble(itemTextBox.Text), Convert.ToDouble(weightTextBox.Text)))
+            {
+                MessageBox.Show("Your given amount can't be entered because it will be overflown.");
+                itemTextBox.Clear();
+                weightTextBox.Clear();
+                return;
+            }
+
+            itemTextBox.Clear();
+            weightTextBox.Clear();
+
+            currentWeightTextBox.Text = refrigerator.GetCurrentWeight().ToString();
+            remainingWeightTextBox.Text = refrigerator.RemainingWeight().ToString();
+
+            //MessageBox.Show(refrigerator.GetItems().ToString()+", "+ refrigerator.GetUnit().ToString()+", "+ refrigerator.GetWeight().ToString()+"/[Max Weight]");
+            if (i == 1)
+            {
+                i++;
+                showTotal = showTotal + "No Of Item \t" + "Weight/Unit \t" + "Total Weight\n";
+            }
+
+            showTotal = showTotal + refrigerator.GetItems().ToString() + "\t\t" + refrigerator.GetUnit().ToString() + "\t\t" + refrigerator.GetWeight().ToString() + "/[Max Weight]" + "\n";
+            showRichTextBox.Text = showTotal;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            refrigerator = new Refrigerator(Convert.ToDouble(maxWeightTakeTextBox.Text));
+            maxWeightTakeTextBox.Clear();
+        }
+    }
+}
